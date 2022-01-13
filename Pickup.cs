442c315b998f6bd4b6bcb235288da76b5eb2655d
QuasiVerse <54774805+QuasiVerse@users.noreply.github.com>
2022-01-12 23:53:47 -0600

    Add files via upload
    Added pickup class

---
+++ Pickup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    
+    private CookingManager manager;
+    
+    public int pickuptype;
+    
+    void Start()
+    {
+        manager = GameObject.Find("Main Camera").GetComponent<CookingManager>();
+    }
+    
+    public void UsePickup(int playernumber, PlayerController playercontroller)
+    {
+        if(pickuptype == 1)
+        {
+            playercontroller.speedboosttimer = 50f;
+        }
+        if(pickuptype == 2)
+        {
+            if(playernumber == 1)
+            {
+                manager.playeronetimer = manager.playeronetimer + 25;
+            }
+            if(playernumber == 2)
+            {
+                manager.playertwotimer = manager.playertwotimer + 25;
+            }
+        }
+        if(pickuptype == 3)
+        {
+            manager.Report(1, playernumber, 15);
+        }
+        Destroy(this.gameObject);
+    }
+}
