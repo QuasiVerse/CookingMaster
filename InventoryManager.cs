7135d1e380d8ed8d7f3e6ff56c16a07364ee146c
QuasiVerse <54774805+QuasiVerse@users.noreply.github.com>
2022-01-12 23:44:18 -0600

    Update InventoryManager.cs
    Added player plates, chopped items, and inventory ui

--- InventoryManager.cs
+++ InventoryManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,7 +7,10 @@ using UnityEngine.UI;
 public class InventoryManager : MonoBehaviour
 {
     public List<string> playerinventory;
-    public List<string> playerchoppingtableinventory;
+    public List<string> playerchoppedinventory;
+    public List<string> playerchoppedtableinventory;
+    public string playerchoppingtableitem = "";
+    public string playerplateitem = "";
     public int playernumber;
     
     private List<Image> playeroneslotone;
@@ -15,11 +18,21 @@ public class InventoryManager : MonoBehaviour
     private List<Image> playertwoslotone;
     private List<Image> playertwoslottwo;
     
+    private List<GameObject> playeronechoppingmodels;
+    private List<GameObject> playertwochoppingmodels;
+    
+    private List<GameObject> playeronechoppedinventoryslot;
+    private List<GameObject> playertwochoppedinventoryslot;
+    
+    private List<GameObject> playeroneplatemodels;
+    private List<GameObject> playertwoplatemodels;
+
     private GenerateHUD hud;
     
     void Start(){
         playerinventory = new List<string>();
-        playerchoppingtableinventory = new List<string>();
+        playerchoppedinventory = new List<string>();
+        playerchoppedtableinventory = new List<string>();
         
         hud = GameObject.Find("Main Camera").GetComponent<GenerateHUD>();
         playeroneslotone = new List<Image>();
@@ -53,12 +66,55 @@ public class InventoryManager : MonoBehaviour
         playertwoslottwo.Add(hud.playertwoslottwopotato);
         playertwoslottwo.Add(hud.playertwoslottwocorn);
         playertwoslottwo.Add(hud.playertwoslottwolettuce);
+        
+        playeronechoppingmodels = new List<GameObject>();
+        playeronechoppingmodels.Add(LevelGenerator.playeronechoppingeggplantmodel);
+        playeronechoppingmodels.Add(LevelGenerator.playeronechoppingtomatomodel);
+        playeronechoppingmodels.Add(LevelGenerator.playeronechoppingzuccinimodel);
+        playeronechoppingmodels.Add(LevelGenerator.playeronechoppingpotatomodel);
+        playeronechoppingmodels.Add(LevelGenerator.playeronechoppingcornmodel);
+        playeronechoppingmodels.Add(LevelGenerator.playeronechoppinglettucemodel);
+        
+        playertwochoppingmodels = new List<GameObject>();
+        playertwochoppingmodels.Add(LevelGenerator.playertwochoppingeggplantmodel);
+        playertwochoppingmodels.Add(LevelGenerator.playertwochoppingtomatomodel);
+        playertwochoppingmodels.Add(LevelGenerator.playertwochoppingzuccinimodel);
+        playertwochoppingmodels.Add(LevelGenerator.playertwochoppingpotatomodel);
+        playertwochoppingmodels.Add(LevelGenerator.playertwochoppingcornmodel);
+        playertwochoppingmodels.Add(LevelGenerator.playertwochoppinglettucemodel);
+        
+        playeronechoppedinventoryslot = new List<GameObject>();
+        playeronechoppedinventoryslot.Add(LevelGenerator.playeronefirstchoppedmodel);
+        playeronechoppedinventoryslot.Add(LevelGenerator.playeronesecondchoppedmodel);
+        playeronechoppedinventoryslot.Add(LevelGenerator.playeronethirdchoppedmodel);
+        
+        playertwochoppedinventoryslot = new List<GameObject>();
+        playertwochoppedinventoryslot.Add(LevelGenerator.playertwofirstchoppedmodel);
+        playertwochoppedinventoryslot.Add(LevelGenerator.playertwosecondchoppedmodel);
+        playertwochoppedinventoryslot.Add(LevelGenerator.playertwothirdchoppedmodel);
+        
+        playeroneplatemodels = new List<GameObject>();
+        playeroneplatemodels.Add(LevelGenerator.playeroneplateeggplantmodel);
+        playeroneplatemodels.Add(LevelGenerator.playeroneplatetomatomodel);
+        playeroneplatemodels.Add(LevelGenerator.playeroneplatezuccinimodel);
+        playeroneplatemodels.Add(LevelGenerator.playeroneplatepotatomodel);
+        playeroneplatemodels.Add(LevelGenerator.playeroneplatecornmodel);
+        playeroneplatemodels.Add(LevelGenerator.playeroneplatelettucemodel);
+        
+        playertwoplatemodels = new List<GameObject>();
+        playertwoplatemodels.Add(LevelGenerator.playertwoplateeggplantmodel);
+        playertwoplatemodels.Add(LevelGenerator.playertwoplatetomatomodel);
+        playertwoplatemodels.Add(LevelGenerator.playertwoplatezuccinimodel);
+        playertwoplatemodels.Add(LevelGenerator.playertwoplatepotatomodel);
+        playertwoplatemodels.Add(LevelGenerator.playertwoplatecornmodel);
+        playertwoplatemodels.Add(LevelGenerator.playertwoplatelettucemodel);
     }
     
     void Update()
     {
         if(playernumber == 1)
         {
+            //Player One Inventory UI
             for(int x = 0; x < playeroneslotone.Count; x++)
             {
                 if(playerinventory.Count == 0)
@@ -91,9 +147,70 @@ public class InventoryManager : MonoBehaviour
                     }
                 }
             }
+            //Player One Chopping Table Item
+            for(int x = 0; x < playeronechoppingmodels.Count; x++)
+            {
+                if(playeronechoppingmodels[x].name == playerchoppingtableitem)
+                {
+                    playeronechoppingmodels[x].SetActive(true);
+                }
+                if(playeronechoppingmodels[x].name != playerchoppingtableitem)
+                {
+                    playeronechoppingmodels[x].SetActive(false);
+                }
+            }
+            //Player One Chopped Items
+            for(int x = 0; x < 3; x++)
+            {
+                if(playerchoppedtableinventory.Count > x)
+                {
+                    playeronechoppedinventoryslot[x].SetActive(true); 
+                    if(playerchoppedtableinventory[x] == "Eggplant")
+                    {
+                        playeronechoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.eggplantcolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Tomato")
+                    {
+                        playeronechoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.tomatocolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Zuccini")
+                    {
+                        playeronechoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.zuccinicolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Potato")
+                    {
+                        playeronechoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.potatocolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Corn")
+                    {
+                        playeronechoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.corncolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Lettuce")
+                    {
+                        playeronechoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.lettucecolor);
+                    }
+                }
+                if(playerchoppedtableinventory.Count <= x)
+                {
+                    playeronechoppedinventoryslot[x].SetActive(false); 
+                }
+            }
+            //Player One Plate Item
+            for(int x = 0; x < playeroneplatemodels.Count; x++)
+            {
+                if(playeroneplatemodels[x].name == playerplateitem)
+                {
+                    playeroneplatemodels[x].SetActive(true);
+                }
+                if(playeroneplatemodels[x].name != playerplateitem)
+                {
+                    playeroneplatemodels[x].SetActive(false);
+                }
+            }
         }
         if(playernumber == 2)
         {
+            //Player Two Inventory UI
             for(int x = 0; x < playertwoslotone.Count; x++)
             {
                 if(playerinventory.Count == 0)
@@ -124,6 +241,66 @@ public class InventoryManager : MonoBehaviour
                     {
                         playertwoslottwo[x].enabled = false;
                     }
+                }
+            }
+            //Player Two Chopping Table Item
+            for(int x = 0; x < playertwochoppingmodels.Count; x++)
+            {
+                if(playertwochoppingmodels[x].name == playerchoppingtableitem)
+                {
+                    playertwochoppingmodels[x].SetActive(true);
+                }
+                if(playertwochoppingmodels[x].name != playerchoppingtableitem)
+                {
+                    playertwochoppingmodels[x].SetActive(false);
+                }
+            }
+            //Player Two Chopped Item
+            for(int x = 0; x < 3; x++)
+            {
+                if(playerchoppedtableinventory.Count > x)
+                {
+                    playertwochoppedinventoryslot[x].SetActive(true); 
+                    if(playerchoppedtableinventory[x] == "Eggplant")
+                    {
+                        playertwochoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.eggplantcolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Tomato")
+                    {
+                        playertwochoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.tomatocolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Zuccini")
+                    {
+                        playertwochoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.zuccinicolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Potato")
+                    {
+                        playertwochoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.potatocolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Corn")
+                    {
+                        playertwochoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.corncolor);
+                    }
+                    if(playerchoppedtableinventory[x] == "Lettuce")
+                    {
+                        playertwochoppedinventoryslot[x].GetComponent<Renderer>().material.SetColor("_Color", LevelGenerator.lettucecolor);
+                    }
+                }
+                if(playerchoppedtableinventory.Count <= x)
+                {
+                    playertwochoppedinventoryslot[x].SetActive(false); 
+                }
+            }
+            //Player Two Plate Item
+            for(int x = 0; x < playertwoplatemodels.Count; x++)
+            {
+                if(playertwoplatemodels[x].name == playerplateitem)
+                {
+                    playertwoplatemodels[x].SetActive(true);
+                }
+                if(playertwoplatemodels[x].name != playerplateitem)
+                {
+                    playertwoplatemodels[x].SetActive(false);
                 }
             }
         }
